7e1b784e2b14663280cbead8e26fec5c29c27f77
DevvratBani <130656998+DevvratBani@users.noreply.github.com>
2023-11-10 14:49:26 +0530

    Update UGV.ino

--- UGV.ino
+++ UGV.ino
@@ -1,17 +1,19 @@
-#include <Encoder.h> // this maynot be used
-
 #include <nRF24L01.h> 
 #include <printf.h>
 #include <RF24.h>
 #include <RF24_config.h>
-
 #include <SPI.h>
 #include <Wire.h>
-#include <Servo.h>
-#include <Adafruit_MotorShield.h>
 
-RF24 radio(9, 10); // CE, CSN pins
-const byte address[6] = "00001";
+#include <Servo.h>
+#include <Encoder.h> 
+#include <Adafruit_MotorShield.h>
+#include <Adafruit_PWMServoDriver.h>
+
+
+RF24 radio(9, 10); // Define the NRF24 module pins (CE, CSN)
+
+const uint64_t pipeAddress = 0xF0F0F0F0E1LL; // Receiver pipe address, should match the transmitter
 
 Adafruit_MotorShield AFMS1 = Adafruit_MotorShield(); // First Motor Shield
 Adafruit_MotorShield AFMS2 = Adafruit_MotorShield(); // Second Motor Shield
@@ -19,40 +21,48 @@ Adafruit_MotorShield AFMS2 = Adafruit_MotorShield(); // Second Motor Shield
 Adafruit_StepperMotor *stepper = AFMS1.getStepper(200, 1);
 Adafruit_DCMotor      *leftMotor = AFMS1.getMotor(3);
 Adafruit_DCMotor      *rightMotor = AFMS1.getMotor(4);
-Adafruit_DCMotor      *oppositeTurnMotor = AFMS2.getMotor(1);
+Adafruit_DCMotor      *turret = AFMS2.getMotor(1);
 
 Servo flexServo;
 
-int data[4];
+struct SensorData {
+  int xAxisValue;
+  int yAxisValue;
+  int steps;
+  int flexSensorValue;
+};
 
-void setup() 
-{
-  AFMS1.begin();
-  AFMS2.begin();
-  stepper->setSpeed(100); // Adjust the speed as needed based on your motor
-  
+SensorData sensorReading;
+int lastEncoderPosition = 0;
+
+void setup() {
   Serial.begin(9600);
   radio.begin();
-  radio.openReadingPipe(1, address);
+  radio.openReadingPipe(1, pipeAddress);
+  radio.setPALevel(RF24_PA_HIGH);
   radio.startListening();
+
+  AFMS1.begin();
+  AFMS2.begin();
+  stepper->setSpeed(10); // Adjust the speed as needed based on your motor
   flexServo.attach(10); // Servo motor pin
 }
 
-void loop() 
-{
-  if (radio.available()) 
-  {
-    int data[4] = {0};
-    radio.read(&data, sizeof(data));
+void loop() {
+  // Receive sensor data from the transmitter
+  if (radio.available()) {
+    radio.read(&sensorReading, sizeof(SensorData));
+    
+    // Use sensorReading values to control the motors
 
-    int xAxisValue = data[0];
-    int yAxisValue = data[1];
-    int steps      = data[2];
-    int flexSensorValue = data[3];
+    int xAxisValue = sensorReading.xAxisValue; 
+    int yAxisValue = sensorReading.yAxisValue; 
+    int steps = sensorReading.steps;
+    int flexSensorValue = sensorReading.flexSensorValue;
 
-    if (radio.available()) {
-    radio.read(&data, sizeof(data));
-    Serial.println("Data received: " + String(data[0]) + ", " + String(data[1]) + ", " + String(data[2]) + ", " + String(data[3]));}
+    // Read the current position of the encoder
+    
+  
 
     leftMotor->setSpeed(abs(yAxisValue)); // try to put actual speed value here, OR
     rightMotor->setSpeed(abs(yAxisValue)); // try to put setSpeed(yAxisValue), OR 
@@ -80,14 +90,39 @@ void loop()
     
     // Use the received step count to control the stepper motor
     // For example, move the motor by the received steps
-    stepper->step(steps, FORWARD, SINGLE);
+    int currentEncoderPosition = steps;
+
+  // Check if the encoder position has changed by 1 value
+  if (currentEncoderPosition != lastEncoderPosition) {
+    if (currentEncoderPosition - lastEncoderPosition == 1) {
+      // If the encoder has been turned by 1 value clockwise, move the stepper motor 4 steps clockwise
+      stepper->step(4, FORWARD, SINGLE);  // Adjust the number of steps and mode as needed
+    } else if (currentEncoderPosition - lastEncoderPosition == -1) {
+      // If the encoder has been turned by 1 value counterclockwise, move the stepper motor 4 steps counterclockwise
+      stepper->step(4, BACKWARD, SINGLE);  // Adjust the number of steps and mode as needed
+    }
+
+    // Update the last encoder position
+    lastEncoderPosition = currentEncoderPosition;
+  }
 
     // Control the servo motor based on flex sensor input
     flexServo.write(flexSensorValue);
 
     // troubleshoot this turret too
-    turret->setSpeed(abs(motorspeed); //this liine wasnt her, but just experimenting
-                                       //it needs to be calibrated anyways 
+    turret->setSpeed(abs(xAxisValue)); //this liine wasnt her, but just experimenting,it needs to be calibrated anyways                                   
     turret->run(xAxisValue < 0 ? FORWARD : BACKWARD);
-  }  
+   
+  
+    // Print received data to Serial monitor
+    Serial.print("Received Data: ");
+    Serial.print(sensorReading.xAxisValue);
+    Serial.print(", ");
+    Serial.print(sensorReading.yAxisValue);
+    Serial.print(", ");
+    Serial.print(sensorReading.steps);
+    Serial.print(", ");
+    Serial.println(sensorReading.flexSensorValue);
+  
+  }
 }
